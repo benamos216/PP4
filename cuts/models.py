5e2c3a4d0414ef9699d2d5246b6f9156765f1478
benamos216 <benamos216@gmail.com>
2022-11-17 10:20:13 +0000

    setting up models

--- cuts/models.py
+++ cuts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Supplier (models.Model):
+    supplier = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.supplier
+    
+
+class Range (models.Model):
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name='Supplier')
+    range = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.range
+
+
+class RollCut (models.Model):
+    range = models.ForeignKey(Range, on_delete=models.CASCADE, related_name='Range')
+    roll_colour = models.CharField(max_length=20, null=False, blank=False)
+    roll_width = models.CharField(max_length=2, null=False, blank=False, default='4m')
+    roll_length = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False, default='25.00')
+    location = models.CharField(max_length=2, null=False, blank=False)
+    invoice = 
+    cut_size = models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False)
+    cut = models.BooleanField(null=False, blank=False, default=False)
+    date_cut = models.DateTimeField(auto_now_add=True)
+    cut_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.environ['DATABASE_URL']='postgres://nuahzwxv:5od4OfCrnEcwNDpR6h1isDVlofK8W5Na@surus.db.elephantsql.com/nuahzwxv'
+os.environ['SECRET_KEY']='ktyp1s8_%6*6#g6752hlkp$769_k1od5qh_rs89*8b4-pi$f#s'
+os.environ['CLOUDINARY_URL']='cloudinary://542198291661993:F2j7DzGGfZNIvK-65xEE4CEguNQ@dw88orkqf'
